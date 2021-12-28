decd404caedff1962075b03498d74075223d98bb
joshuanieh <b07502027@ntu.edu.tw>
2021-12-28 17:18:13 +0800

    new

--- server/server.cpp
+++ server/server.cpp
@@ -23,6 +23,7 @@ int main(int argc, char const *argv[]) {
 	vector<string> filelist;
 	streampos begin, end, base[max_number_of_users];
 	bool flag;
+	char httpMessage[4096] = "";
 	int o = true, j;
 	fstream file;
 	long long filesize[max_number_of_users];
@@ -66,126 +67,132 @@ int main(int argc, char const *argv[]) {
         }
         for(int i = 0; i < max_number_of_users; i++) {
             if(FD_ISSET(sockets[i], &readfds)) {
-                if(recv(sockets[i], buff, 5, MSG_WAITALL) <= 0) {
+                if(recv(sockets[i], buff, 6, MSG_WAITALL) <= 0) {
                     close(sockets[i]);
                     sockets[i] = 0;
                     username[i] = "";
                 }
                 else{
-					cout << buff << endl;
-                	flag = true;
-                	if(strcmp(buff, "lsaf") == 0) {
-		                for (const auto &n: filesystem::directory_iterator{root/username[i]}) filelist.push_back(n.path().filename().string());
-					    sort(filelist.begin(), filelist.end());
-					    for (const string &name: filelist) {
-					    	strcat(buff, name.c_str());
-					    	strcat(buff, "\n");
-					    	// cout << file.path().filename().string() << endl;
-					    }
-					    filelist.clear();
-					    send(sockets[i], buff, buff_len, MSG_NOSIGNAL);
+                	strcat(httpMessage, buff);
+                	size_t len = strlen(httpMessage);
+                	if(httpMessage[len - 1] == '\n' && httpMessage[len - 2] == '\r') {
+                		cout << httpMessage << endl;
+                		strcpy(httpMessage, "");
                 	}
-                    if(strcmp(buff, "lsss") == 0) {
-                    	// cout << root << endl;
-					    for (const auto &n: filesystem::directory_iterator{root}) filelist.push_back(n.path().filename().string());
-					    sort(filelist.begin(), filelist.end());
-					    for (const string &name: filelist) {
-					    	strcat(buff, name.c_str());
-					    	strcat(buff, "\n");
-					    	// cout << file.path().filename().string() << endl;
-					    }
-					    filelist.clear();
-					    send(sockets[i], buff, buff_len, MSG_NOSIGNAL);
-                    }
-                    else if(strcmp(buff, "putt") == 0) {
-                    	if(recv(sockets[i], buff, buff_len, MSG_WAITALL) <= 0) {
-		                    close(sockets[i]);
-		                    sockets[i] = 0;
-		                    username[i] = "";
-		                }
-		                else {
-		                	filename[i] = root/(string)buff;
-		                	file.open(filename[i], ios::out|ios::binary);
-		                	file.close();
-		                	if(recv(sockets[i], buff, buff_len, MSG_WAITALL) <= 0) {
-			                    close(sockets[i]);
-			                    sockets[i] = 0;
-			                    username[i] = "";
-			                }
-			                else filesize[i] = atoll(buff);
-		                }
-                    }
-                    else if(strcmp(buff, "puti") == 0) {
-                    	if(recv(sockets[i], buff, buff_len, MSG_WAITALL) <= 0) {
-		                    close(sockets[i]);
-		                    sockets[i] = 0;
-		                    username[i] = "";
-		                }
-		                else {
-		                	if(filesize[i] >= buff_len) {
-		                		file.open(filename[i], ios::out|ios::binary|ios::app);
-			                	file.write(buff, buff_len);
-			                	file.close();
-			                	filesize[i] -= buff_len;
-		                	}
-		                	else {
-		                		file.open(filename[i], ios::out|ios::binary|ios::app);
-			                	file.write(buff, filesize[i]);
-			                	file.close();
-		                	}
-		                }
-                    }
-                    else if(strcmp(buff, "gett") == 0) {
-                    	base[i] = 0;
-                    	if(recv(sockets[i], buff, buff_len, MSG_WAITALL) <= 0) {
-		                    close(sockets[i]);
-		                    sockets[i] = 0;
-		                    username[i] = "";
-		                }
-		                else {
-		                	filename[i] = root/(string)buff;
-		                	file.open(filename[i], ios::in|ios::binary);
-		                	if(file.is_open()) {
-		                		send(sockets[i], "The file exists.", buff_len, MSG_NOSIGNAL);
-		                		begin = file.tellg();
-								file.seekg(0, ios::end);
-								end = file.tellg();
-		                		send(sockets[i], to_string(end - begin).c_str(), buff_len, MSG_NOSIGNAL);
-		                		file.close();
-		                	}
-			                else send(sockets[i], "The file doesn't exists.", buff_len, MSG_NOSIGNAL);
-		                }
-                    }
-                    else if(strcmp(buff, "geti") == 0) {
-                    	memset(buff, '\0', buff_len);
-	                	file.open(filename[i], ios::in|ios::binary);
-                		file.seekg(base[i], ios::beg);
-                		file.read(buff, buff_len);
-					    base[i] = file.tellg();
-					    file.close();
-					    // send(sockets[i], to_string(j).c_str(), buff_len, MSG_NOSIGNAL);
-	                	// cout << buff << endl;
-	                	send(sockets[i], buff, buff_len, MSG_NOSIGNAL);
-	                	// if(file.peek() == EOF) file.close();
-                    }
-                    else if(strcmp(buff, "logi") == 0) {
-                    	memset(buff, '\0', buff_len);
-                    	if(recv(sockets[i], buff, buff_len, MSG_WAITALL) <= 0) {
-		                    close(sockets[i]);
-		                    sockets[i] = 0;
-		                    username[i] = "";
-		                }
-                    	for(int k = 0; k < max_number_of_users; k++) {
-                    		if(strcmp(buff, username[k].c_str()) == 0) {
-                    			send(sockets[i], "username is in used, please try another:", buff_len, MSG_NOSIGNAL);
-                    			flag = false;
-                    			break;
-                    		}
-                    	}
-                    	if(!flag) continue;
-                    	send(sockets[i], "connect successfully", buff_len, MSG_NOSIGNAL);
-                    	username[i] = buff;
-                    }
+                	// cout << buff << endl;
+                	// flag = true;
+        //         	if(strcmp(buff, "lsaf") == 0) {
+		      //           for (const auto &n: filesystem::directory_iterator{root/username[i]}) filelist.push_back(n.path().filename().string());
+					   //  sort(filelist.begin(), filelist.end());
+					   //  for (const string &name: filelist) {
+					   //  	strcat(buff, name.c_str());
+					   //  	strcat(buff, "\n");
+					   //  	// cout << file.path().filename().string() << endl;
+					   //  }
+					   //  filelist.clear();
+					   //  send(sockets[i], buff, buff_len, MSG_NOSIGNAL);
+        //         	}
+        //             if(strcmp(buff, "lsss") == 0) {
+        //             	// cout << root << endl;
+					   //  for (const auto &n: filesystem::directory_iterator{root}) filelist.push_back(n.path().filename().string());
+					   //  sort(filelist.begin(), filelist.end());
+					   //  for (const string &name: filelist) {
+					   //  	strcat(buff, name.c_str());
+					   //  	strcat(buff, "\n");
+					   //  	// cout << file.path().filename().string() << endl;
+					   //  }
+					   //  filelist.clear();
+					   //  send(sockets[i], buff, buff_len, MSG_NOSIGNAL);
+        //             }
+        //             else if(strcmp(buff, "putt") == 0) {
+        //             	if(recv(sockets[i], buff, buff_len, MSG_WAITALL) <= 0) {
+		      //               close(sockets[i]);
+		      //               sockets[i] = 0;
+		      //               username[i] = "";
+		      //           }
+		      //           else {
+		      //           	filename[i] = root/(string)buff;
+		      //           	file.open(filename[i], ios::out|ios::binary);
+		      //           	file.close();
+		      //           	if(recv(sockets[i], buff, buff_len, MSG_WAITALL) <= 0) {
+			     //                close(sockets[i]);
+			     //                sockets[i] = 0;
+			     //                username[i] = "";
+			     //            }
+			     //            else filesize[i] = atoll(buff);
+		      //           }
+        //             }
+        //             else if(strcmp(buff, "puti") == 0) {
+        //             	if(recv(sockets[i], buff, buff_len, MSG_WAITALL) <= 0) {
+		      //               close(sockets[i]);
+		      //               sockets[i] = 0;
+		      //               username[i] = "";
+		      //           }
+		      //           else {
+		      //           	if(filesize[i] >= buff_len) {
+		      //           		file.open(filename[i], ios::out|ios::binary|ios::app);
+			     //            	file.write(buff, buff_len);
+			     //            	file.close();
+			     //            	filesize[i] -= buff_len;
+		      //           	}
+		      //           	else {
+		      //           		file.open(filename[i], ios::out|ios::binary|ios::app);
+			     //            	file.write(buff, filesize[i]);
+			     //            	file.close();
+		      //           	}
+		      //           }
+        //             }
+        //             else if(strcmp(buff, "gett") == 0) {
+        //             	base[i] = 0;
+        //             	if(recv(sockets[i], buff, buff_len, MSG_WAITALL) <= 0) {
+		      //               close(sockets[i]);
+		      //               sockets[i] = 0;
+		      //               username[i] = "";
+		      //           }
+		      //           else {
+		      //           	filename[i] = root/(string)buff;
+		      //           	file.open(filename[i], ios::in|ios::binary);
+		      //           	if(file.is_open()) {
+		      //           		send(sockets[i], "The file exists.", buff_len, MSG_NOSIGNAL);
+		      //           		begin = file.tellg();
+								// file.seekg(0, ios::end);
+								// end = file.tellg();
+		      //           		send(sockets[i], to_string(end - begin).c_str(), buff_len, MSG_NOSIGNAL);
+		      //           		file.close();
+		      //           	}
+			     //            else send(sockets[i], "The file doesn't exists.", buff_len, MSG_NOSIGNAL);
+		      //           }
+        //             }
+        //             else if(strcmp(buff, "geti") == 0) {
+        //             	memset(buff, '\0', buff_len);
+	       //          	file.open(filename[i], ios::in|ios::binary);
+        //         		file.seekg(base[i], ios::beg);
+        //         		file.read(buff, buff_len);
+					   //  base[i] = file.tellg();
+					   //  file.close();
+					   //  // send(sockets[i], to_string(j).c_str(), buff_len, MSG_NOSIGNAL);
+	       //          	// cout << buff << endl;
+	       //          	send(sockets[i], buff, buff_len, MSG_NOSIGNAL);
+	       //          	// if(file.peek() == EOF) file.close();
+        //             }
+        //             else if(strcmp(buff, "logi") == 0) {
+        //             	memset(buff, '\0', buff_len);
+        //             	if(recv(sockets[i], buff, buff_len, MSG_WAITALL) <= 0) {
+		      //               close(sockets[i]);
+		      //               sockets[i] = 0;
+		      //               username[i] = "";
+		      //           }
+        //             	for(int k = 0; k < max_number_of_users; k++) {
+        //             		if(strcmp(buff, username[k].c_str()) == 0) {
+        //             			send(sockets[i], "username is in used, please try another:", buff_len, MSG_NOSIGNAL);
+        //             			flag = false;
+        //             			break;
+        //             		}
+        //             	}
+        //             	if(!flag) continue;
+        //             	send(sockets[i], "connect successfully", buff_len, MSG_NOSIGNAL);
+        //             	username[i] = buff;
+        //             }
                     // else cout << "Forbidden area: " << buff << endl;
                 }
             }
