0351bc7df20350d54322c0640314baa6a53f20f4
joshuanieh <b07502027@ntu.edu.tw>
2022-01-12 10:49:18 +0800

    new

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,11 @@ function App() {
   const { status, opened, messages, sendMessage, clearMessages } = useChat()
 
   const [username, setUsername] = useState('')
+  const [options, setOptions] = useState(false)
+  const [chatRoom, setChatRoom] = useState(false)
+  const [addFriend, setAddFriend] = useState(false)
+  const [removeFriend, setRemoveFriend] = useState(false)
+  const [listFriends, setListFriends] = useState(false)
   const [body, setBody] = useState('')
 
   const bodyRef = useRef(null)
@@ -40,43 +45,82 @@ function App() {
 
   return (
     <div className="App">
-      {!username ? 
-        <Input.Search
-          placeholder="Choose a username..."
-          enterButton="Continue"
-          ref={bodyRef}
-          style={{ marginBottom: 10 }}
-          onSearch={(msg) => {
-            !msg ? 
-              displayStatus({
-                type: 'error',
-                msg: 'Please enter a username.'
-              }) :
-              setUsername(msg)
-              sendMessage("Hello")
-          }}
-        ></Input.Search> :
-        <div>
+      {!options ?
+        <div> 
           <div className="App-title">
-            <h1>Simple Chat</h1>
+            <h1>Login</h1>
           </div>
-          <div className="App-messages">
-            {messages.length === 0 ? (
-              <p style={{ color: '#ccc' }}>
-                {opened? 'No messages...' : 'Loading...'}
-              </p>
-            ) : (
-              messages.map(({ name, body }, i) => (
-                <p className="App-message" key={i}>
-                  <Tag color="blue">{name}</Tag> {body}
+          <Input.Search
+            placeholder="Please enter a username:"
+            enterButton="Continue"
+            ref={bodyRef}
+            style={{ marginBottom: 10 }}
+            onChange={(e) => setUsername(e.target.value)}
+            onSearch={(msg) => {
+              !msg ? 
+                displayStatus({
+                  type: 'error',
+                  msg: 'Please enter a username.'
+                }) 
+                : 
+                sendMessage(`Login {username}`)
+                setOptions(true)
+            }}>
+            </Input.Search>
+        </div> 
+        : 
+        <>
+        {addFriend | removeFriend | listFriends | chatRoom ?
+          <div>
+            <div className="App-title">
+              <h1>Chat</h1>
+            </div>
+            <div className="App-messages">
+              {messages.length === 0 ? (
+                <p style={{ color: '#ccc' }}>
+                  {opened? 'No messages...' : 'Loading...'}
                 </p>
-              ))
-            )}
-          </div>
-          <Button type="primary" danger onClick={clearMessages}>
-            Clear
-          </Button>
-        </div>
+              ) : (
+                messages.map(({ name, body }, i) => (
+                  <p className="App-message" key={i}>
+                    <Tag color="blue">{name}</Tag> {body}
+                  </p>
+                ))
+              )}
+            </div>
+            <Button type="primary" danger onClick={clearMessages}>
+              Clear
+            </Button>
+           </div>
+          : 
+          <>
+            <div className="App-title">
+              <h1>Options</h1>
+            </div>
+            <Button type="primary" style={{margin: '20px'}} danger onClick={() => {
+              sendMessage(`List friends`)
+              setListFriends(true)
+            }}>
+              List all friends
+            </Button>
+            <Button type="primary" style={{margin: '20px'}} danger onClick={() => {
+              setAddFriend(true)
+            }}>
+              Add a friend
+            </Button>
+            <Button type="primary" style={{margin: '20px'}} danger onClick={() => {
+              setRemoveFriend(true)
+            }}>
+              Remove a friend
+            </Button>
+            <Button type="primary" style={{margin: '20px'}} danger onClick={() => {
+              setChatRoom(true)
+            }}>
+              Chat with
+            </Button>
+          </>
+        }
+        </>
       }
     </div>
   )
