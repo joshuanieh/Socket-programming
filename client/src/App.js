ffe182c4c4c50b057a85c45b58ed327865544a39
joshuanieh <b07502027@ntu.edu.tw>
2022-01-12 18:20:09 +0800

    new

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
 import './App.css'
+
 import React, { useEffect, useRef, useState } from 'react'
-import useChat from './useChat'
+// import useChat from './useChat'
 import { Button, Input, message, Tag } from 'antd'
 
 function App() {
-  const { status, opened, messages, sendMessage, clearMessages } = useChat()
+  // const { status, opened, messages, sendMessage, clearMessages } = useChat()
+  
+  // const http = new XMLHttpRequest()
+  // http.open("POST", "https://localhost:4000", true)
+  // http.setRequestHeader('Content-Type', 'text/plain;charset=UTF-8')
+  var http = require('http');
+  // var agent = new http.Agent({ keepAlive: true }); // false by default
+  
+  
 
+  // http.request({
+  //     host: '127.0.0.1',
+  //     port: 4000,
+  //     method: 'POST',
+  //     agent: agent, // use this agent for more requests as needed
+  //     data: 'lll'
+  // }).end();
+  const [messages, setMessages] = useState("")
+  const [textFinish, setTextFinish] = useState("")
   const [username, setUsername] = useState('')
   const [options, setOptions] = useState(false)
   const [chatRoom, setChatRoom] = useState(false)
@@ -39,9 +57,9 @@ function App() {
     }
   }
 
-  useEffect(() => {
-    displayStatus(status)
-  }, [status])
+  // useEffect(() => {
+  //   displayStatus(status)
+  // }, [status])
 
   return (
     <div className="App">
@@ -57,68 +75,183 @@ function App() {
             style={{ marginBottom: 10 }}
             onChange={(e) => setUsername(e.target.value)}
             onSearch={(msg) => {
-              !msg ? 
+              if(msg) {
+                  ////////////////////////// sendMessage(`Login {username}`)
+                  setOptions(true)
+              }
+              else {
                 displayStatus({
                   type: 'error',
                   msg: 'Please enter a username.'
-                }) 
-                : 
-                sendMessage(`Login {username}`)
-                setOptions(true)
+                })
+              } 
             }}>
             </Input.Search>
         </div> 
         : 
         <>
         {listFriends ?
-          <div>
+          <>
             <div className="App-title">
               <h1>All friends</h1>
             </div>
-            <div className="App-messages">
-              {messages.length === 0 ? (
-                <p style={{ color: '#ccc' }}>
-                  {opened? 'No messages...' : 'Loading...'}
-                </p>
-              ) : (
-                <p className="App-message">
-                  <Tag color="blue">{messages}</Tag>
-                </p>
-              )}
+            <div>
+              <p className="App-message">
+                <Tag color="blue">{messages}</Tag>
+              </p>
             </div>
             <Button type="primary" danger onClick={() => {
               setListFriends(false)
             }}>
               Leave
             </Button>
-           </div>
+          </>
           : 
           <>
-            <div className="App-title">
-              <h1>Options</h1>
-            </div>
-            <Button type="primary" style={{margin: '20px'}} danger onClick={() => {
-              sendMessage(`List friends`)
-              setListFriends(true)
-            }}>
-              List all friends
-            </Button>
-            <Button type="primary" style={{margin: '20px'}} danger onClick={() => {
-              setAddFriend(true)
-            }}>
-              Add a friend
-            </Button>
-            <Button type="primary" style={{margin: '20px'}} danger onClick={() => {
-              sendMessage(`List friends`)
-              setRemoveFriend(true)
-            }}>
-              Remove a friend
-            </Button>
-            <Button type="primary" style={{margin: '20px'}} danger onClick={() => {
-              setChatRoom(true)
-            }}>
-              Chat with
-            </Button>
+            {addFriend ? 
+              <>
+                <div className="App-title">
+                  <h1>Add friends</h1>
+                </div>
+                <Input.Search
+                  placeholder="Please enter a username:"
+                  enterButton="Add"
+                  ref={bodyRef}
+                  style={{ marginBottom: 10 }}
+                  onChange={(e) => setUsername(e.target.value)}
+                  onSearch={(msg) => {
+                    if(msg) {
+                        ////////////////////// sendMessage(`Add {username}`)
+                    }
+                    else {
+                      displayStatus({
+                        type: 'error',
+                        msg: 'Please enter a username.'
+                      })
+                    } 
+                  }}>
+                </Input.Search>
+                <Button type="primary" danger onClick={() => {
+                  setAddFriend(false)
+                }}>
+                  Leave
+                </Button>
+              </>
+              :
+              <>
+                {removeFriend ? 
+                  <>
+                    <div className="App-title">
+                      <h1>Remove friends</h1>
+                    </div>
+                    <Input.Search
+                      placeholder="Please enter a username:"
+                      enterButton="Remove"
+                      ref={bodyRef}
+                      style={{ marginBottom: 10 }}
+                      onChange={(e) => setUsername(e.target.value)}
+                      onSearch={(msg) => {
+                        if(msg) {
+                            //////////////////sendMessage(`Remove {username}`)
+                        }
+                        else {
+                          displayStatus({
+                            type: 'error',
+                            msg: 'Please enter a username.'
+                          })
+                        }
+                      }}>
+                    </Input.Search>
+                    <Button type="primary" danger onClick={() => {
+                      setRemoveFriend(false)
+                    }}>
+                      Leave
+                    </Button>
+                  </>
+                  :
+                  <>
+                    {chatRoom ? 
+                      <>
+                        <div className="App-title">
+                          <h1>Chat room</h1>
+                        </div>
+                        <div>
+                          <p className="App-message">
+                            <Tag color="blue">{messages}</Tag>
+                          </p>
+                        </div>
+                        <Button type="primary" danger onClick={() => {
+                          setChatRoom(false)
+                        }}>
+                          Leave
+                        </Button>
+                      </>
+                      :
+                      <>
+                        <div className="App-title">
+                          <h1>Options</h1>
+                        </div>
+                        <Button type="primary" style={{margin: '20px'}} danger onClick={() => {
+                          /////////////////////sendMessage('List friends')
+                          // http.onload = () => console.log(http.responseText)
+                          // http.send('List friends')
+                          //////////////////////////
+                          // const da = JSON.stringify({
+                          //   todo: 'Buy the milk'
+                          // })
+                          const da = "HAHA"
+                          const option = {
+                            hostname: '127.0.0.1',
+                            port: 4000,
+                            method: 'POST',
+                            headers: {
+                              'Content-Type': 'text/plain',
+                              'Content-Length': da.length
+                            }
+                          }
+
+                          const req = http.request(option, res => {
+                            console.log(`statusCode: ${res.statusCode}`)
+
+                            res.on('data', d => {
+                              process.stdout.write(d)
+                            })
+                          })
+
+                          req.on('error', error => {
+                            console.error(error)
+                          })
+
+                          req.write(da)
+                          req.end()
+                          /////////////////////////////
+                          setListFriends(true)
+                        }}>
+                          List all friends
+                        </Button>
+                        <Button type="primary" style={{margin: '20px'}} danger onClick={() => {
+                          setAddFriend(true)
+                        }}>
+                          Add friends
+                        </Button>
+                        <Button type="primary" style={{margin: '20px'}} danger onClick={() => {
+                          // sendMessage(`List friends`)
+                          setRemoveFriend(true)
+                        }}>
+                          Remove friends
+                        </Button>
+                        <Button type="primary" style={{margin: '20px'}} danger onClick={() => {
+                          //////////////sendMessage('List friends')
+                          setChatRoom(true)
+                        }}>
+                          Chat with
+                        </Button>
+                      </>
+                    }
+                  </>
+                }
+              </>
+            }
           </>
         }
         </>
