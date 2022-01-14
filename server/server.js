1cddb0c3c99858a0ad86529936e21273e9583ed8
joshuanieh <b07502027@ntu.edu.tw>
2022-01-14 18:17:36 +0800

    new

--- server/server.js
+++ server/server.js
@@ -3,9 +3,11 @@ var fs = require('fs');
  
 var server = http.createServer(function (req, res) {
     req.on('data', (msg) => {
-        let data = msg.toString('utf8').split(' ')
+        let message = msg.toString('utf8')
+        let fileName = message.substr(0, message.indexOf(' '))
+        let content = message.substr(message.indexOf(' ') + 1)
         console.log(msg.toString('utf8'))
-        fs.writeFile(`./client_dir/${data[0]}`, data[1], (err) => {
+        fs.writeFile(`./client_dir/${fileName}`, content, (err) => {
             console.log(err)
         })
     })
